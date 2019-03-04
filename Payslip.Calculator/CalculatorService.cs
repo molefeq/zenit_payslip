4a89be9fd8af66b67efb0c77c08a4c80d4043d08
bbdnet1168 <bbdnet1168@QINISELAM>
2019-03-04 10:44:55 +0200

    this commit will create the initial files repos.

---
+++ Payslip.Calculator/CalculatorService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Payslip.Calculator
+{
+    public class CalculatorService : ICalculateService
+    {
+        public decimal CalculateAnnualGrossRemuneration()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal CalculateAnnualRetirementFundsContributions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal CalculateGrossRemuneration()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal CalculateLimitAnnualRetirementFundsContributions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal CalculateLimitBasedOnSalaryAnnualRetirementFundsContributions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal CalculateNetPay()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal CalculatePaye()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal CalculateRetirementfundsContributionDeductions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal CalculateRetirementFundsContributions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal CalculateTaxableIncome()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal CalculateTotalMedicalAidTaxCredits()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal CalculateUif()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Payslip.Calculator/ICalculateService.cs
@@ -0,0 +1,18 @@
+﻿namespace Payslip.Calculator
+{
+    public interface ICalculateService
+    {
+        decimal CalculateGrossRemuneration();
+        decimal CalculateAnnualGrossRemuneration();
+        decimal CalculateRetirementFundsContributions();
+        decimal CalculateAnnualRetirementFundsContributions();
+        decimal CalculateLimitAnnualRetirementFundsContributions();
+        decimal CalculateLimitBasedOnSalaryAnnualRetirementFundsContributions();
+        decimal CalculateRetirementfundsContributionDeductions();
+        decimal CalculateTaxableIncome();
+        decimal CalculateTotalMedicalAidTaxCredits();
+        decimal CalculatePaye();
+        decimal CalculateUif();
+        decimal CalculateNetPay();
+    }
+}
